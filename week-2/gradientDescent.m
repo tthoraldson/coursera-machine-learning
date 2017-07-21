ceec630051a661d844a6dda7b04f38b7a6f69439
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-07-21 11:35:55 -0500

    completed gradient descent function

--- week-2/gradientDescent.m
+++ week-2/gradientDescent.m
@@ -16,17 +16,16 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    theta;  
+    x = X(:, 2);
+    h = theta(1) + (theta(2)*x);
+    thetaOne = theta(1) - alpha * (1/m) * sum(h - y);
+    thetaTwo = theta(2) - alpha * (1/m) * sum((h - y) .* x);
+    theta = [thetaOne; thetaTwo];
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+    J_history(iter) = computeCost(X, y, theta)
 
 end
 
